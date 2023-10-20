ee24db0cef43bebc713ad1c461b14e5a47eb4566
wetPaint99 <benedictjoseph67@gmail.com>
2023-10-20 14:22:27 +0000

    Update

--- _printf.c
+++ _printf.c
@@ -1,63 +1,51 @@
 #include "main.h"
 
-/**
- * _printf - Printf function
- * @format: format.
- * Return: Printed chars.
- */
 int _printf(const char *format, ...)
 {
-	int printed_char = 0;
+    int printed_char = 0;
 
-	va_list lists;
+    va_list lists;
+    va_start(lists, format);
 
-	if (*format == NULL)
-	{
-		return (-1);
-	}
+    if (format == NULL)
+    {
+        return (-1);
+    }
 
-	va_start(lists, format);
-	while (*format)
-	{
-		if (format != '%')
-		{
-			write(1, format, 1);
-			printed_char++;
-		}
-		else
-		{
-			format++;
-			if (*format == '\0')
-			break;
-			if (*format == '%')
-			{
-				write(1, format, 1);
-				printed_char++;
-			}
-			else if (*format == 'c')
-			{
-				char c = va_arg(lists, int);
+    while (*format)
+    {
+        if (*format != '%')
+        {
+            write(1, format, 1);
+            printed_char++;
+        }
+        else
+        {
+            format++;
+            if (*format == '\0')
+                break;
+            if (*format == '%')
+            {
+                write(1, format, 1);
+                printed_char++;
+            }
+            else if (*format == 'c')
+            {
+                char c = va_arg(lists, int);
+                write(1, &c, 1);
+                printed_char++;
+            }
+            else if (*format == 's')
+            {
+                char *hen = va_arg(lists, char *);
+                int str_len = strlen(hen); // Use strlen to get the string length
+                write(1, hen, str_len);
+                printed_char += str_len;
+            }
+        }
+        format++;
+    }
 
-				write(1, &c, 1);
-				printed_char++;
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(lists, char*);
-				int str_len = 0;
-
-				while (str(str_len) != '\0')
-				{
-					str_len++;
-				}
-				write(1, str, str_len);
-				printed_char += str_len;
-			}
-		}
-		format++;
-	}
-
-	va_end(lists);
-
-	return (printed_char);
+    va_end(lists);
+    return (printed_char);
 }
